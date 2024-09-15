e1e4740418e8b32b64e753a2b7a8bdf25addfc0c
kstbyev <madi2005@bk.ru>
2024-09-15 21:18:12 +0500

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Madi Sharipov on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
